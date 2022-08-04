1746992b6332ded64427a589e2e37002a6543efa
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-04 20:01:57 +0300

    feat: Implemented register functionality, partial user context, fixed DB URL

--- src/providers/AppContext.ts
+++ src/providers/AppContext.ts
@@ -1,1 +1,12 @@
-export {};
+import { createContext, type Dispatch } from 'react';
+
+
+const AppState = createContext({
+  appState: {
+    user: null,
+    userData: null,
+  },
+  setState: (() => undefined) as Dispatch<any>,
+});
+
+export default AppState;
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,1 +1,13 @@
-export {};
+import { set, ref, get } from 'firebase/database';
+import { db } from '../config/firebase-config';
+
+export const createUserByUsername= (firstName: string, lastName: string, phoneNumber: string,
+  username: string, email: string | null, uid: string) => {
+  return set(ref(db, `users/${username}`),
+    { firstName, lastName, phoneNumber, username, email, uid, teams: [], channels: [], friends: [] },
+  ).catch((e) => console.log(e));
+};
+
+export const getUserByUsername = (username: string) => {
+  return get(ref(db, `users/${username}`));
+};
