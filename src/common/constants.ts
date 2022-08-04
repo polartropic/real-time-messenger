12b304c0142ebd8f6be57f8e0ab22290735ca849
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-04 21:06:21 +0300

    feat: Add constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,7 @@
-export {};
+export const MIN_USERNAME_LENGTH = 5;
+export const MAX_USERNAME_LENGTH = 35;
+
+export const MIN_TITLE_LENGTH = 3;
+export const MAX_TITLE_LENGTH = 40;
+
+export const MIN_PASSWORD_LENGTH = 6;
