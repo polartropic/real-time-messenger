60ad5f66577a9a70f3f8974a4cbd2960389c50cf
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-04 16:40:38 +0300

    chore: Fix linting issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,22 +2,20 @@ import DefaultAvatar from '../../assets/images/Default-avatar.jpg';
 import './Header.css';
 
 const Header = (): JSX.Element => {
+  return (
+    <header id='header'>
+      <div id='stats'>
+        <h4 className='stats'>Total active users: 10</h4>
+        <h4 className='stats'>Total active teams: 5</h4>
+      </div>
+      <div id='navigation'>
+        <button className='header-btn'>About us</button>
+        <button className='header-btn'>My teams</button>
+        <img className="default-avatar" src={DefaultAvatar} alt="default-avatar" />
+      </div>
 
-    return (
-        <header id='header'>
-            <div id='stats'>
-            <h4 className='stats'>Total active users: 10</h4> 
-            <h4 className='stats'>Total active teams: 5</h4>
-            </div>
-            <div id='navigation'>
-            <button className='header-btn'>About us</button>
-            <button className='header-btn'>My teams</button>
-            <img className="default-avatar" src={DefaultAvatar} alt="default-avatar" />
-            </div>
-
-        </header>
-    )
-        
-}
+    </header>
+  );
+};
 
 export default Header;
