6ae49afa3ac671a6569f9c6ffc5c6e965fc0dc29
Emil Botev <emilebotev@gmail.com>
2022-08-04 15:09:20 +0300

    firebase-config is ready

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,1 +1,29 @@
-export {}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBexIgJ5Ih1psxKlcEZ6m6Zavsxqzc0a84",
+  authDomain: "thunderteam-99849.firebaseapp.com",
+  projectId: "thunderteam-99849",
+  storageBucket: "thunderteam-99849.appspot.com",
+  messagingSenderId: "97084125410",
+  appId: "1:97084125410:web:c06c2cafc47d26cf31eebb",
+  databaseURL: "https://console.firebase.google.com/project/thunderteam-99849/database/thunderteam-99849-default-rtdb/data/~2F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getDatabase(app);
+
+export const storage = getStorage(app);
+
