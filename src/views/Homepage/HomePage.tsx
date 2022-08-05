4fef8d5bf04575c09f27c9da3672da406445d1c9
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-05 10:07:24 +0300

    fix: Moved register to views folder

--- src/views/Homepage/HomePage.tsx
+++ src/views/Homepage/HomePage.tsx
@@ -2,7 +2,7 @@ import './HomePage.css';
 import { useContext } from 'react';
 import AppContext from '../../providers/AppContext';
 import LoggedUser from '../LoggedUser/LoggedUser';
-import Register from '../../components/Register/Register';
+import Register from '../../views/Register/Register';
 
 const HomePage = (): JSX.Element => {
   const { appState } = useContext(AppContext);
