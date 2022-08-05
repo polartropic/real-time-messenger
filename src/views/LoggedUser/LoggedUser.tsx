d38a1b95ecccfe00f6d2eeeac165bc2d073632b5
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-06 01:15:22 +0300

    chore: Remove unnecessary divs

--- src/views/LoggedUser/LoggedUser.tsx
+++ src/views/LoggedUser/LoggedUser.tsx
@@ -5,44 +5,32 @@ const LoggedUser = (): JSX.Element => {
   return (
     <div className="landing-page">
       <div className="chats-channels-list">
-        <div className="chats-channels-inner">
-          <h4>Chats:</h4>
-          <p>User1</p>
-          <p>User2</p>
-          <p>User3</p>
-          <p>User4</p>
-        </div>
-      </div>
-
-      <div className="main-container">
-        <button>View all users</button>
-
         <div className="search-users">
           <input type="text" defaultValue="search users..." />
+          <button className="view-users-btn">View all users</button>
         </div>
 
-        <hr />
+        <h4>Chats:</h4>
+        <p>User1</p>
+        <p>User2</p>
+        <p>User3</p>
+        <p>User4</p>
+      </div>
 
-        {/* DYNAMIC DIV TO SHOW RESULTS FROM SEARCH AND VIEWING CHATS */}
-        <div className="content">
-          <h4>Click around to see your chats, teams or channels show up here.</h4>
-          <Channel />
-        </div>
+      {/* DYNAMIC DIV TO SHOW RESULTS FROM SEARCH AND VIEWING CHATS */}
+      <div className="main-container">
+        <Channel />
       </div>
 
       <div className="participants-list">
-        <div className="owner-inner">
-          <h4>Owner:</h4>
-          <p>User0</p>
-        </div>
+        <h4>Owner:</h4>
+        <p>User0</p>
 
-        <div className="participants-inner">
-          <h4>Participants of chat/team:</h4>
-          <p>User1</p>
-          <p>User2</p>
-          <p>User3</p>
-          <p>User4</p>
-        </div>
+        <h4>Participants of chat/team:</h4>
+        <p>User1</p>
+        <p>User2</p>
+        <p>User3</p>
+        <p>User4</p>
 
         <div className="manage-participants-btns">
           <button className="add-btn"><span>Add members</span></button>
