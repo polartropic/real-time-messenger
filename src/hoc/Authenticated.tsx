bee351068c8703ce01333e5914480942e85b6236
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-05 10:28:15 +0300

    feat: Added Authenticated HOC (to be used for guarded routes)

---
+++ src/hoc/Authenticated.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import AppContext from '../providers/AppContext';
+import { Navigate, useLocation } from 'react-router-dom';
+import { ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode
+}
+
+const Authenticated = ({ children }: Props) => {
+  const { appState } = useContext(AppContext);
+  const user = appState.user;
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/home" state={{ from: location }} />;
+  }
+
+  return children;
+};
+
+export default Authenticated;
