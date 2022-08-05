69f69e24f7d5006005f61ab92862500ff15a3715
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-05 22:50:42 +0300

    style: Change style of chats/participants lists & add/leave btns

--- src/views/LoggedUser/LoggedUser.tsx
+++ src/views/LoggedUser/LoggedUser.tsx
@@ -1,14 +1,17 @@
+import Channel from '../Channel/Channel';
 import './LoggedUser.css';
 
 const LoggedUser = (): JSX.Element => {
   return (
     <div className="landing-page">
       <div className="chats-channels-list">
-        <h4>Chats:</h4>
-        <p>User1</p>
-        <p>User2</p>
-        <p>User3</p>
-        <p>User4</p>
+        <div className="chats-channels-inner">
+          <h4>Chats:</h4>
+          <p>User1</p>
+          <p>User2</p>
+          <p>User3</p>
+          <p>User4</p>
+        </div>
       </div>
 
       <div className="main-container">
@@ -23,22 +26,29 @@ const LoggedUser = (): JSX.Element => {
         {/* DYNAMIC DIV TO SHOW RESULTS FROM SEARCH AND VIEWING CHATS */}
         <div className="content">
           <h4>Click around to see your chats, teams or channels show up here.</h4>
+          <Channel />
         </div>
       </div>
 
       <div className="participants-list">
-        <h4>Owner:</h4>
-        <p>User0</p>
+        <div className="owner-inner">
+          <h4>Owner:</h4>
+          <p>User0</p>
+        </div>
 
-        <h4>Participants of chat/team:</h4>
-        <p>User1</p>
-        <p>User2</p>
-        <p>User3</p>
-        <p>User4</p>
+        <div className="participants-inner">
+          <h4>Participants of chat/team:</h4>
+          <p>User1</p>
+          <p>User2</p>
+          <p>User3</p>
+          <p>User4</p>
+        </div>
 
-        <button>Add more participants</button>
-        <br /><br />
-        <button>Leave chat/channel</button>
+        <div className="manage-participants-btns">
+          <button className="add-btn"><span>Add members</span></button>
+          <br />
+          <button className="leave-btn">Leave channel</button>
+        </div>
       </div>
     </div>
   );
