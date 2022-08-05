90cd4d66898caa8533e1f8750373ea7f577cba64
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-05 17:46:06 +0300

    feat: Added personal details to be visible in edit profile

--- src/views/EditProfile/EditProfile.tsx
+++ src/views/EditProfile/EditProfile.tsx
@@ -1,35 +1,38 @@
 import './EditProfile.css';
 import DefaultAvatar from '../../assets/images/Default-avatar.jpg';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import AppContext from '../../providers/AppContext';
 
 const EditProfile = (): JSX.Element => {
   const navigate = useNavigate();
-
+  const { appState} = useContext(AppContext);
+  const user = appState.userData;
   return (
     <div className="edit-profile">
       <div className="edit-profile-forms">
         <h3 id="edit-profile-title">Personal details</h3>
         <img className="default-avatar" src={DefaultAvatar} alt="default-avatar" />
         <form className="edit-form">
-          <label className="labels-edit" htmlFor="first-name">First Name:</label> <br />
+          <label className="labels-edit" htmlFor="first-name">First Name:<br />  {user?.['firstName']}</label> <br />
           <input type="text" id="first-name" placeholder="first name"/>
           <input type="submit" className="change-button-edit" value="Change" />
         </form>
         <br />
         <form className="edit-form">
-          <label className="labels-edit" htmlFor="last-name">Last name: </label> <br />
+          <label className="labels-edit" htmlFor="last-name">Last name: <br /> {user?.['lastName']}</label> <br />
           <input type="text" id="last-name" placeholder="last name"/>
           <input type="submit" className="change-button-edit" value="Change" />
         </form>
         <br />
         <form className="edit-form">
-          <label className="labels-edit" htmlFor="last-name">Phone number: </label> <br />
+          <label className="labels-edit" htmlFor="last-name">Phone number: <br /> {user?.['phoneNumber']} </label> <br />
           <input type="text" id="last-name" placeholder="phone number" />
           <input type="submit" className="change-button-edit" value="Change" />
         </form>
         <br />
         <form className="edit-form">
-          <label className="labels-edit" htmlFor="e-mail">E-mail: </label> <br />
+          <label className="labels-edit" htmlFor="e-mail">E-mail: <br />  {user?.['email']}</label> <br />
           <input type="email" id="e-mail" placeholder="e-mail" />
           <input type="submit" className="change-button-edit" value="Change" />
         </form>
