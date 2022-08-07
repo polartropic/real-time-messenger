35e7aa78c435ae2fe0fe9cdcca91670183f1c05a
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-07 16:00:50 +0300

    feat: Made search and view all users button display users in logged user view

--- src/views/LoggedUser/LoggedUser.tsx
+++ src/views/LoggedUser/LoggedUser.tsx
@@ -1,14 +1,51 @@
 // import CreateTeam from '../Create-a-team/CreateTeam';
 // import Channel from '../Channel/Channel';
 import './LoggedUser.css';
+import { User } from '../../types/Interfaces';
+import { useEffect, useState } from 'react';
+import { getAllUsers } from '../../services/users.services';
+import UserComponent from '../../components/User/User';
+// import Channel from '../Channel/Channel';
 
 const LoggedUser = (): JSX.Element => {
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isAllUsersClicked, setisAllUsersClicked] = useState(false);
+
+  useEffect(() => {
+    getAllUsers()
+      .then((snapshot) => setAllUsers(Object.values(snapshot.val())));
+  }, []);
+
+  const getUsersBySearchTerm = (searchTerm: string, users: User[]) => {
+    return users.filter((user) =>
+      user.username.toLowerCase().includes(searchTerm) ||
+      user.email.toLowerCase().includes(searchTerm) ||
+      user.firstName.toLowerCase().includes(searchTerm) ||
+      user.lastName.toLowerCase().includes(searchTerm));
+  };
+
+  const result = getUsersBySearchTerm(searchTerm, allUsers);
+
+
+  const mappingResult = (user: User): JSX.Element | undefined => {
+    const buttonEl: JSX.Element =
+      <button onClick={() => {}}
+        id='add-remove-user-btn'>
+        <img src="https://img.icons8.com/color/48/000000/add--v1.png" alt='add-btn' />
+      </button>;
+    return <>
+      <UserComponent props={{ user, buttonEl }} key={user.uid} />
+      <br />
+    </>;
+  };
+
   return (
     <div className="landing-page">
       <div className="chats-channels-list">
         <div className="search-users">
-          <input type="text" defaultValue="search users..." />
-          <button className="view-users-btn">View all users</button>
+          <input type="text" defaultValue="" placeholder='search users...' onChange={(event) => setSearchTerm(event.target.value)}/>
+          <button className="view-users-btn" onClick={() =>setisAllUsersClicked(!isAllUsersClicked)}>View all users</button>
         </div>
 
         <h4>Chats:</h4>
@@ -20,8 +57,19 @@ const LoggedUser = (): JSX.Element => {
 
       {/* DYNAMIC DIV TO SHOW RESULTS FROM SEARCH AND VIEWING CHATS */}
       <div className="main-container">
-        {/* <CreateTeam/> */}
-        {/* <Channel /> */}
+        <>
+          {searchTerm !== '' ?
+            result.map(mappingResult) :
+            null
+          }
+          {isAllUsersClicked ?
+            allUsers.map(mappingResult) :
+            null
+
+          }
+          {/* <CreateTeam/> */}
+          {/* <Channel /> */}
+        </>
       </div>
 
       <div className="participants-list">
