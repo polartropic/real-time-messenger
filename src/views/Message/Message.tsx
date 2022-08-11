db090c36e2856edeae3cf0355626dc5b81dc419d
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-11 15:57:38 +0300

    style: Add conditional style of messages

--- src/views/Message/Message.tsx
+++ src/views/Message/Message.tsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react';
 import DefaultAvatar from '../../assets/images/Default-avatar.jpg';
+import AppContext from '../../providers/AppContext';
 import { MessageProps } from '../../types/Interfaces';
 import './Message.css';
 
 const Message = ({ message }: MessageProps): JSX.Element => {
+  const { appState } = useContext(AppContext);
+  const currentUser = appState.userData;
+  let msgBoxClass = '';
+
+  if (message.author === currentUser?.username) {
+    msgBoxClass = 'message-1';
+  } else {
+    msgBoxClass = 'message-2';
+  }
+
   return (
-    <div id='message' className='message'>
+    <div id='message' className={msgBoxClass}>
       <div className='message-header'>
         <img src={DefaultAvatar} alt='avatar' className='default-avatar' />
         @{message.author}
