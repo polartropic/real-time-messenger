3d948f66c17fa51b4a44c066c4a2e03d5bef9e2a
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-16 14:48:17 +0300

    feat: Modified create meeting function in meeting services

--- src/services/meetings.services.ts
+++ src/services/meetings.services.ts
@@ -21,12 +21,13 @@ export const getMeetingByID = (id: string | null) => {
     });
 };
 
-export const createMeeting = (title: string, start: string, end: string, participants: string[]) => {
+export const createMeeting = (title: string, start: string, end: string, participants: string[], id: string) => {
   return push(ref(db, 'meetings'), {
     title,
     start,
     end,
     participants,
+    id,
   })
     .then((res) => getMeetingByID(res.key));
 };
