4c38758b9da3a40dafd5ddf21c1f49d259d1b15c
Emil Botev <emilebotev@gmail.com>
2022-08-23 22:49:29 +0300

    style: tooltip design implemented and added info

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -2,8 +2,8 @@ import { FC, useState } from 'react';
 // import AppContext from '../../providers/AppContext';
 import './User.css';
 import { UserProps } from '../../types/Interfaces';
-import { Tooltip } from '@mui/material';
 import InitialsAvatar from 'react-initials-avatar';
+import { Tooltip } from '@mui/material';
 
 const UserComponent: FC<UserProps> = ({ props }): JSX.Element => {
   const [open, setOpen] = useState(false);
@@ -15,15 +15,42 @@ const UserComponent: FC<UserProps> = ({ props }): JSX.Element => {
   const handleOpen = () => {
     setOpen(true);
   };
+  const toolTipStyle = {
+    bgcolor: 'red',
+    boxShadow: 1,
+    borderRadius: 20,
+    p: 2,
+    minWidth: 300,
+  };
+
   return (
 
-    <Tooltip open={open} onClose={handleClose} onOpen={handleOpen}
+    <Tooltip open={open} onClose={handleClose} onOpen={handleOpen} className='toolTip'
       title={
-        <div>
-          <div>{props.user.username}</div>
+        <>
+          <p>{props.user.firstName} {props.user.lastName}</p>
           <p>{props.user.email}</p>
-        </div>
-      }>
+          <p>{props.user.phoneNumber}</p>
+
+        </>
+      }
+      placement={'left'}
+      arrow
+      componentsProps={{
+        tooltip: {
+          sx: {
+            'color': 'white',
+            'marginTop': '10px !important',
+            'bgcolor': 'rgba(47, 69, 93, 0.8)',
+            'borderRadius': '10px',
+            'fontSize': 10,
+            'z-index': 'tool-tip: 1000',
+            '& .MuiTooltip-arrow': {
+              'color': 'rgba(47, 69, 93, 0.8)',
+            },
+          },
+        },
+      }}>
       <div className="user-box" id="user-box">
         {props?.user.imgURL ?
           <div>
