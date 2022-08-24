757f8adf5179995f63266ebbf7ae76a4cc9a0ffb
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-24 14:23:45 +0300

    chore: Update userstatus to actual enum

--- src/common/user-status.enum.ts
+++ src/common/user-status.enum.ts
@@ -5,3 +5,11 @@ export const userStatus = {
   'IN_A_MEETING': 'in a meeting',
   'AWAY': 'away',
 };
+
+export enum UserStatus {
+  OFFLINE = 'offline',
+  ONLINE = 'online',
+  DO_NOT_DISTURB = 'do not disturb',
+  IN_A_MEETING = 'in a meeting',
+  AWAY = 'away',
+}
