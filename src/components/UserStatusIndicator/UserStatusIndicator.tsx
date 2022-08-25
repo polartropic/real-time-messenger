298ac577c59a7a6db16afa6da46daec2d2cbf733
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-25 03:12:02 +0300

    feat: Set colour of do not disturb and in a meeting status

--- src/components/UserStatusIndicator/UserStatusIndicator.tsx
+++ src/components/UserStatusIndicator/UserStatusIndicator.tsx
@@ -15,8 +15,10 @@ const UserStatusIndicator = ({ user }: UserStatusProps) => {
     const unsubscribe = getLiveStatus(user.username, (snapshot) => {
       const currentStatusInDB = snapshot.val();
 
-      if (currentStatusInDB === UserStatusEnum.ONLINE) setColor('green');
-      if (currentStatusInDB === UserStatusEnum.AWAY) setColor('yellow');
+      if (currentStatusInDB === UserStatusEnum.ONLINE) setColor('#83D350');
+      if (currentStatusInDB === UserStatusEnum.AWAY) setColor('#FBCE02');
+      if (currentStatusInDB === UserStatusEnum.DO_NOT_DISTURB) setColor('#BC0000');
+      if (currentStatusInDB === UserStatusEnum.IN_A_MEETING) setColor('#680003');
       if (currentStatusInDB === UserStatusEnum.OFFLINE) setColor('gray');
     });
 
