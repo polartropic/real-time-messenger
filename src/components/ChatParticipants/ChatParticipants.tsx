8ed7b1632838106c72d4426a43d51e247b9d31aa
Teodora Yaneva <theodorayaneva@gmail.com>
2022-08-25 03:24:27 +0300

    chore: Fix formatting in ChatParticipants.tsx

--- src/components/ChatParticipants/ChatParticipants.tsx
+++ src/components/ChatParticipants/ChatParticipants.tsx
@@ -8,9 +8,9 @@ import { useContext, useState } from 'react';
 import AppContext from '../../providers/AppContext';
 import { uid } from 'uid';
 import DateTimePicker from 'react-datetime-picker';
-import './ChatParticipants.css';
 import { API_KEY, BASE_URL, ORGANIZATION_ID } from '../../common/constants';
 import UserComponent from '../User/User';
+import './ChatParticipants.css';
 
 const ChatParticipants = ({ currentChannel, allUsers, owner }: ChatParticipantsProps): JSX.Element | null => {
   const { appState,
@@ -41,6 +41,7 @@ const ChatParticipants = ({ currentChannel, allUsers, owner }: ChatParticipantsP
       .then((response) => {
         const convertedStart = start.toISOString();
         const convertedEnd = end.toISOString();
+
         createMeeting(name, convertedStart, convertedEnd, currentChannel.participants, response.data.data.meeting.id)
           .then(() => toast.success('Successfully scheduled meeting!'));
       })
@@ -70,22 +71,25 @@ const ChatParticipants = ({ currentChannel, allUsers, owner }: ChatParticipantsP
     if (setIsCreateChatClicked) {
       setIsCreateChatClicked(false);
     }
+
     if (isTeamView) {
       setIsTeamView(true);
     }
   };
+
   const currentChannelUsers = allUsers
     .filter((user) => currentChannel.participants.includes(user.username));
+
   return (
     isDetailedChatClicked ?
-      <div className="participants-list">
-        <button onClick={() => setIsMeetingClicked(!isMeetingClicked)} className="view-users-btn">Create a meeting</button>
+      <div className='participants-list'>
+        <button onClick={() => setIsMeetingClicked(!isMeetingClicked)} className='view-users-btn'>Create a meeting</button>
         {isMeetingClicked ?
           <form id='create-a-meeting' onSubmit={handleCreateMeeting}>
             <h4>Create a meeting with chat participants:</h4>
             <p>Meeting name:
               <label htmlFor='content'></label>
-              <input type="text" required placeholder="Input meeting's name" value={name} onChange={(e) => setName(e.target.value)} />
+              <input type='text' required placeholder="Input meeting's name" value={name} onChange={(e) => setName(e.target.value)} />
             </p>
             <p id='meeting-start'>Meeting start:
               <DateTimePicker onChange={setStart} value={start} />
@@ -93,7 +97,7 @@ const ChatParticipants = ({ currentChannel, allUsers, owner }: ChatParticipantsP
             <p id='meeting-end'>Meeting end:
               <DateTimePicker onChange={setEnd} value={end} />
             </p>
-            <button className="view-users-btn">Schedule meeting</button>
+            <button className='view-users-btn'>Schedule meeting</button>
           </form> :
           null
         }
@@ -103,16 +107,16 @@ const ChatParticipants = ({ currentChannel, allUsers, owner }: ChatParticipantsP
           {currentChannelUsers.map((participant) => mappingParticipants(participant, uid()))}
         </div>
 
-        <div className="manage-participants-btns">
+        <div className='manage-participants-btns'>
           {URL.includes('/teams/') ?
-            <button className="add-btn" onClick={() => loadTeamDetails()}><span>Team details</span></button> :
+            <button className='add-btn' onClick={() => loadTeamDetails()}><span>Team details</span></button> :
             null}
           <br />
-          <button onClick={() => leaveChat()} className="leave-btn">Leave channel</button>
+          <button onClick={() => leaveChat()} className='leave-btn'>Leave channel</button>
         </div>
         <ToastContainer />
       </div> :
-      <div className="participants-list">
+      <div className='participants-list'>
       </div>
   );
 };
