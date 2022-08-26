16e018f054fce302f9fa600f91e0362b79ebb401
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-26 20:47:53 +0300

    feat: Added links to Gitlab profiles in about us and added a picture

--- src/views/AboutUs/AboutUs.tsx
+++ src/views/AboutUs/AboutUs.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import DefaultAvatar from '../../assets/images/Default-avatar.jpg';
+import Gitlab from '../../assets/images/Gitlab-icon.png';
+import Stefani from '../../assets/images/Stefani.jpg';
 import './AboutUs.css';
 
 const AboutUs = (): JSX.Element => {
@@ -15,34 +17,27 @@ const AboutUs = (): JSX.Element => {
 
       <div className='descriptions'>
         <div className='single'>
-          <img src={DefaultAvatar} alt='Avatar of creator' />
-          <h4>Emil Botev</h4>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <img className='avatar-about-us' src={DefaultAvatar} alt='Avatar of creator' />
+          <h3>Emil Botev</h3>
+          <a href="https://gitlab.com/emilebotev" target="_blank" rel="noreferrer noopener">
+            <img className='gitlab-icon' src={Gitlab} alt="gitlab-icon" />
+          </a>
         </div>
 
         <div className='single'>
-          <img src={DefaultAvatar} alt='Avatar of creator' />
-          <h4>Stefani Staneva</h4>
-          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-            magni dolores eos qui ratione voluptatem sequi nesciunt.
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."</p>
+          <img className='avatar-about-us' src={Stefani} alt='Avatar of creator' />
+          <h3>Stefani Staneva</h3>
+          <a href="https://gitlab.com/StefaniStaneva97" target="_blank" rel="noreferrer noopener">
+            <img className='gitlab-icon' src={Gitlab} alt="gitlab-icon" />
+          </a>
         </div>
 
         <div className='single'>
-          <img src={DefaultAvatar} alt='Avatar of creator' />
-          <h4>Teodora Yaneva</h4>
-          <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
-            deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
-            similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
-            Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est
-            eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-            omnis voluptas assumenda est, omnis dolor repellendus."</p>
+          <img className='avatar-about-us' src={DefaultAvatar} alt='Avatar of creator' />
+          <h3>Teodora Yaneva</h3>
+          <a href="https://gitlab.com/teodorayaneva" target="_blank" rel="noreferrer noopener">
+            <img className='gitlab-icon' src={Gitlab} alt="gitlab-icon" />
+          </a>
         </div>
       </div>
     </div>
