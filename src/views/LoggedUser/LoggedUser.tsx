6b6009add9a0986984d2300be8f65b337bee0f10
Stefani Staneva <StefaniStaneva97@gmail.com>
2022-08-31 15:23:14 +0300

    fix: Fixed bug - logged user was breaking because of change in useState

--- src/views/LoggedUser/LoggedUser.tsx
+++ src/views/LoggedUser/LoggedUser.tsx
@@ -26,7 +26,15 @@ const LoggedUser = (): JSX.Element => {
   } = useContext(AppContext);
   const userDetails: User = appState.userData!;
 
-  const [currentChat, setCurrentChat] = useState<IChannel>({} as IChannel);
+  const [currentChat, setCurrentChat] = useState<IChannel>({
+    id: '',
+    title: '',
+    participants: [],
+    messages: [],
+    isPublic: false,
+    teamID: '',
+    lastActivity: new Date(),
+  });
 
   const [channels, setChannels] = useState<IChannel[]>([]);
   const [allUsers, setAllUsers] = useState<User[]>([]);
@@ -83,6 +91,7 @@ const LoggedUser = (): JSX.Element => {
       .catch(console.error);
   }, []);
 
+  console.log(channels);
 
   const createChatFunc = () => {
     if (title.length < MIN_CHANNEL_NAME_LENGTH || title.length > MAX_CHANNEL_NAME_LENGTH) {
